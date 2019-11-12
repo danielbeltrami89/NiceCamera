9d41bb468ff1905bc01fc3c3281bec3258d1a530
moraesdan89 <moraesdan89@gmail.com>
2019-11-12 10:34:22 -0300

    Create Pod

---
+++ NiceCamera/NiceCamera/NiceCamera.h
@@ -0,0 +1,19 @@
+//
+//  NiceCamera.h
+//  NiceCamera
+//
+//  Created by Informatica Labo Mac on 12/11/19.
+//  Copyright © 2019 Dan Beltrami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NiceCamera.
+FOUNDATION_EXPORT double NiceCameraVersionNumber;
+
+//! Project version string for NiceCamera.
+FOUNDATION_EXPORT const unsigned char NiceCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NiceCamera/PublicHeader.h>
+
+
